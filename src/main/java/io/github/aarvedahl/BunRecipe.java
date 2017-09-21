240a4185e7e15f016ef50c20e22f40dbae2227f6
Alex Arvedahl <alex.arvedahl@gmail.com>
2017-09-21 11:36:24 +0200

    Made bun recipe

--- src/main/java/io/github/aarvedahl/BunRecipe.java
+++ src/main/java/io/github/aarvedahl/BunRecipe.java
@@ -8,6 +8,12 @@ public class BunRecipe implements Recipe{
     @Override
     public List<Ingredient> getRecipe() {
         List<Ingredient> ingredientList = new ArrayList<>();
-
+        Ingredient sugar = new Sugar(0.06);
+        ingredientList.add(sugar);
+        Ingredient milk = new Milk(0.1);
+        ingredientList.add(milk);
+        Ingredient flour = new Flour(0.12);
+        ingredientList.add(flour);
+        return ingredientList;
     }
 }
